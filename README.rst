f64b9c1ae114303d1a21cef9f1e68957c9709b17
Philippe Ombredanne <pombredanne@nexb.com>
2024-08-14 14:40:25 +0200

    Improve settings and README
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- README.rst
+++ README.rst
@@ -6,23 +6,39 @@ FederatedCode is a decentralized, federated metadata system for open source soft
 security information.
 
 
-===================
 Quick Installation
-===================
+--------------------
+
 On a Debian system, use this::
 
-    sudo apt-get install  python3-venv python3-dev postgresql libpq-dev build-essential
-    git clone https://github.com/nexB/federatedcode.git && cd federatedcode
-    make dev envfile postgres
+    sudo apt-get install python3-venv python3-dev postgresql libpq-dev build-essential
+    git clone https://github.com/nexB/federatedcode.git
+    cd federatedcode
+    make dev envfile postgresdb
     make test
     source venv/bin/activate
     make run
 
+Configuration
+-------------------
+
+The configuration of FederatedCode depends on environment variables:
+
+
+- FEDERATEDCODE_WORKSPACE_LOCATION: Directory location of the workspace where we store local Git repos and
+  content. Default to var/ in current directory in development
+- FEDERATEDCODE_CLIENT_ID:
+- FEDERATEDCODE_CLIENT_SECRET: Generated id and secret stored in a .env file 
+
 
 Acknowledgements
 ^^^^^^^^^^^^^^^^
-This project was funded through the NGI0 Entrust Fund, a fund established by NLnet with financial
+
+This project is funded through the NGI0 Entrust Fund, a fund established by NLnet with financial
 support from the European Commission's Next Generation Internet programme, under the aegis of DG
 Communications Networks, Content and Technology under grant agreement No 101069594.
 
 https://nlnet.nl/project/FederatedSoftwareMetadata/
+
+This project also receives support and funding from Google and nexB.
+
--- federatedcode/settings.py
+++ federatedcode/settings.py
@@ -1,54 +1,85 @@
 #
 # Copyright (c) nexB Inc. and others. All rights reserved.
-# VulnerableCode is a trademark of nexB Inc.
+# FederatedCode is a trademark of nexB Inc.
 # SPDX-License-Identifier: Apache-2.0
 # See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
-# See https://github.com/nexB/vulnerablecode for support or download.
-# See https://aboutcode.org for more information about nexB OSS projects.
+# See https://github.com/nexB/federatedcode for support or download.
+# See https://aboutcode.org for more information about AboutCode.org OSS projects.
 #
 import os
+import sys
 from pathlib import Path
 
 import environ
 
 PROJECT_DIR = Path(__file__).resolve().parent
 ROOT_DIR = PROJECT_DIR.parent
+
 # Environment
 
-ENV_FILE = "federated_code/.env"
+ENV_FILE = "/etc/federatedcode/.env"
 if not Path(ENV_FILE).exists():
     ENV_FILE = ROOT_DIR / ".env"
 
 env = environ.Env()
 environ.Env.read_env(str(ENV_FILE))
 
-FEDERATED_CODE_DOMAIN = env.str("FEDERATED_CODE_DOMAIN", "127.0.0.1:8000")
-FEDERATED_CODE_GIT_PATH = env.str("FEDERATED_CODE_GIT_PATH", "")
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Security
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+SECRET_KEY = env.str("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uoc-dv7+6%dy7c6(hc$6*z_m-#4y*jp1%-^*5)y&+i9-@j7zup"
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[".localhost", "127.0.0.1", "[::1]", "host.docker.internal"],
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+# SECURITY WARNING: don't run with debug turned on in production
+DEBUG = env.bool("FEDERATEDCODE_DEBUG", default=False)
+
+
+############################################
+# Federation settings
+AP_CONTENT_TYPE = "application/activity+json"
+
+FEDERATEDCODE_LOG_LEVEL = env.str("FEDERATEDCODE_LOG_LEVEL", "INFO")
+
+# Use True for production
+FEDERATEDCODE_REQUIRE_AUTHENTICATION = env.bool(
+    "FEDERATEDCODE_REQUIRE_AUTHENTICATION", default=False
+)
+# The DNS host, port and "domain"
+FEDERATEDCODE_HOST = env.str("FEDERATEDCODE_HOST", "127.0.0.1")
+FEDERATEDCODE_PORT = env.str("FEDERATEDCODE_PORT", "8000")
+FEDERATEDCODE_DOMAIN = env.str("FEDERATEDCODE_DOMAIN", f"{FEDERATEDCODE_HOST}:{FEDERATEDCODE_PORT}")
+
+# directory location of the workspace where we store Git repos and content
+# default to var/ in current directory in development
+FEDERATEDCODE_WORKSPACE_LOCATION = env.str("FEDERATEDCODE_WORKSPACE_LOCATION", ROOT_DIR / "var")
+
+# these do NOT have a default
+FEDERATEDCODE_CLIENT_ID = env.str("FEDERATEDCODE_CLIENT_ID")
+FEDERATEDCODE_CLIENT_SECRET = env.str("FEDERATEDCODE_CLIENT_SECRET")
+
+FEDERATEDCODE_STATIC_ROOT = "/var/federatedcode/static/"
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    # Local apps
+    # Must come before Third-party apps for proper templates override
+    "fedcode",
+    # Django built-in
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.humanize",
+    # Third-party apps
     "oauth2_provider",
-    "fedcode",
 ]
 
 MIDDLEWARE = [
@@ -59,44 +90,51 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # OAUTH
     "oauth2_provider.middleware.OAuth2TokenMiddleware",
 ]
 
 ROOT_URLCONF = "federatedcode.urls"
 
+WSGI_APPLICATION = "federatedcode.wsgi.application"
+
+SECURE_PROXY_SSL_HEADER = env.tuple(
+    "SECURE_PROXY_SSL_HEADER", default=("HTTP_X_FORWARDED_PROTO", "https")
+)
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": env.str("FEDERATEDCODE_DB_ENGINE", "django.db.backends.postgresql"),
+        "HOST": env.str("FEDERATEDCODE_DB_HOST", "localhost"),
+        "NAME": env.str("FEDERATEDCODE_DB_NAME", "federatedcode"),
+        "USER": env.str("FEDERATEDCODE_DB_USER", "federatedcode"),
+        "PASSWORD": env.str("FEDERATEDCODE_DB_PASSWORD", "federatedcode"),
+        "PORT": env.str("FEDERATEDCODE_DB_PORT", "5432"),
+        "ATOMIC_REQUESTS": True,
+    }
+}
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
+            "debug": DEBUG,
             "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "federatedcode.wsgi.application"
-
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# postgresql database
-DATABASES = {
-    "default": {
-        "ENGINE": env.str("DB_ENGINE", "django.db.backends.postgresql"),
-        "HOST": env.str("POSTGRES_HOST", "127.0.0.1"),
-        "NAME": env.str("POSTGRES_DB_NAME", "federatedcode"),
-        "USER": env.str("POSTGRES_USER", "federatedcode"),
-        "PASSWORD": env.str("POSTGRES_PASSWORD", "federatedcode"),
-        "PORT": env.str("POSTGRES_PORT", "5432"),
-    }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+# Passwords
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,6 +142,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": env.int("FEDERATEDCODE_PASSWORD_MIN_LENGTH", default=12),
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
@@ -113,13 +154,125 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Testing
+
+# True if running tests through `./manage test` or `pytest`
+IS_TESTS = any(t in sys.argv for t in ("test", "pytest"))
+
+if IS_TESTS:
+    # Do not pollute the workspace while running the tests.
+    FEDERATEDCODE_WORKSPACE_LOCATION = tempfile.mkdtemp()
+    FEDERATEDCODE_REQUIRE_AUTHENTICATION = True
+    # The default password hasher is rather slow by design.
+    # Using a faster hashing algorithm in the testing context to speed up the run.
+    PASSWORD_HASHERS = [
+        "django.contrib.auth.hashers.MD5PasswordHasher",
+    ]
+
+# Debug toolbar
+
+DEBUG_TOOLBAR = env.bool("FEDERATEDCODE_DEBUG_TOOLBAR", default=False)
+if DEBUG and DEBUG_TOOLBAR:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = ["127.0.0.1"]
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "null": {
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        "scanpipe": {
+            "handlers": ["null"] if IS_TESTS else ["console"],
+            "level": FEDERATEDCODE_LOG_LEVEL,
+            "propagate": False,
+        },
+        "django": {
+            "handlers": ["null"] if IS_TESTS else ["console"],
+            "propagate": False,
+        },
+        # Set FEDERATEDCODE_LOG_LEVEL=DEBUG to display all SQL queries in the console.
+        "django.db.backends": {
+            "level": FEDERATEDCODE_LOG_LEVEL,
+        },
+    },
+}
+
+# Instead of sending out real emails the console backend just writes the emails
+# that would be sent to the standard output.
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Internationalization
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = env.str("TIME_ZONE", default="UTC")
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = env.str("STATIC_ROOT", default="/var/federatedcode/static/")
+
+STATICFILES_DIRS = [
+    PROJECT_DIR / "static",
+]
+
+
+# Django restframework
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(ROOT_DIR, "media")
+
+
+# Django restframework
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
         "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework.renderers.AdminRenderer",
+    ),
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": env.int("FEDEREATEDCDOE_REST_API_PAGE_SIZE", default=50),
+    "UPLOADED_FILES_USE_URL": False,
 }
+
+
+if not FEDERATEDCODE_REQUIRE_AUTHENTICATION:
+    REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = ("rest_framework.permissions.AllowAny",)
+
 
 OAUTH2_PROVIDER = {
     "ACCESS_TOKEN_EXPIRE_SECONDS": 3600,
@@ -131,28 +284,3 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "oauth2_provider.backends.OAuth2Backend",
 )
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-# STATIC_ROOT = env.str("FEDERATED_CODE_STATIC_ROOT", "./")
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-AP_CONTENT_TYPE = "application/activity+json"
